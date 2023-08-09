b72f0444e8e9bde45c85c24d2c716c409fa7c12d
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-09 17:33:49 +0530

    add discord token

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 from dotenv import load_dotenv
 import youtube_dl
 
+load_dotenv()
+DISCORD_TOKEN = os.getenv("MTEzODc4NzQ1NTYwNTE0OTc5Nw.Gd_uR2.6T6IAjpD_VovI4-D4QQZHeHY7G7mcVyukzetzQ")
 
 intents = discord.Intents().all
 client = discord.Client(intents = intents)
