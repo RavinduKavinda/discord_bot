4c0d518432f2e20b285de034dfd1c68562174f45
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-09 17:32:22 +0530

    define variables

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import os
 from dotenv import load_dotenv
 import youtube_dl
 
+
+intents = discord.Intents().all
+client = discord.Client(intents = intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
