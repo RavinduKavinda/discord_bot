86d1d97e60713a95c9045cc2aa2b52c62abc9cfe
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-09 17:44:17 +0530

    added format

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ DISCORD_TOKEN = os.getenv("MTEzODc4NzQ1NTYwNTE0OTc5Nw.Gd_uR2.6T6IAjpD_VovI4-D4QQ
 intents = discord.Intents().all
 client = discord.Client(intents = intents)
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warning':True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
