347ac378b53acb4856c87c7fb29d04bb45fc3453
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-10 18:00:26 +0530

    added classmethod

--- main.py
+++ main.py
@@ -38,4 +38,14 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.title = data.get('title')
         self.url = ''
 
+    @classmethod
+    async def from_url(cls, url, *, loop = None, stream = False):  
+          loop = loop or asyncio.get_event_loop()
+          data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download = not stream))
+          if 'entries' in data:
+               data = data['entries'][0]
+               filename = data['title'] if stream else ytdl.prepare_filename(data)
+               return filename
+          
+
     
